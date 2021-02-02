a41d26a58fa9c9af1831f2c4240ad521d7a6ab2d
thxnk <58910440+thxnk@users.noreply.github.com>
2021-02-02 10:34:05 +0200

    Update Predator.java

--- src/domain/Predator.java
+++ src/domain/Predator.java
@@ -1,8 +1,14 @@
 package domain;
 
 public class Predator extends Animal {
-
+    
+    protected boolean isInGroop;
+       
     public void hunt() {
         System.out.println("Predator is hunting...");
+    }
+    
+    public void fly() {
+        System.out.println("Predator is flying...");
     }
 }
